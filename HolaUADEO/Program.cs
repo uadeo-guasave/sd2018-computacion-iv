fdfa6a8870e68e4fb0f233e346b60728cd738f23
bidkar <bidkar.aragon@udo.mx>
2018-09-11 11:50:24 -0600

    mapeo de tabla users

--- HolaUADEO/Program.cs
+++ HolaUADEO/Program.cs
@@ -10,6 +10,25 @@ namespace HolaUADEO
     {
         static void Main(string[] args)
         {
+            var user1 = new User();
+            user1.FirstName = "Bidkar";
+            user1.LastName = "Aragon";
+            user1.Email = "bidkar.aragon@udo.mx";
+
+            Console.WriteLine("Usuario: " + user1.FullName);
+
+            var user2 = new User
+            {
+                FirstName = "Ofelia",
+                LastName = "La del abarrote",
+                Email = "ofelia@abarrote.santa.fe",
+                Name = "ofelia",
+                Password = "123"
+            };
+
+            Console.WriteLine("Usuario: " + user2.FullName);
+            Console.Read();
+
         }
     }
 }
---
+++ HolaUADEO/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaUADEO
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string RememberToken { get; set; }
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+    }
+}
