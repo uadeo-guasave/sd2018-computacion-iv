ba1035ec53cba904c03550b0fe1b88d2c3ce595c
bidkar <bidkar.aragon@udo.mx>
2018-09-13 11:46:16 -0600

    primer contacto con formularios

---
+++ HolaUADEO/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HolaUADEO
+{
+    class Form1 : Form
+    {
+        public Button btnSaludar;
+        public Form1()
+        {
+            btnSaludar = new Button();
+            btnSaludar.Size = new Size(50, 40);
+            btnSaludar.Location = new Point(30, 30);
+            btnSaludar.Text = "Saluda me";
+            this.Controls.Add(btnSaludar);
+            btnSaludar.Click += new EventHandler(btnSaludar_Click);
+        }
+
+        public void btnSaludar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Formulario desde línea de comandos");
+        }
+    }
+}
--- HolaUADEO/Program.cs
+++ HolaUADEO/Program.cs
@@ -3,32 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace HolaUADEO
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            var user1 = new User();
-            user1.FirstName = "Bidkar";
-            user1.LastName = "Aragon";
-            user1.Email = "bidkar.aragon@udo.mx";
-
-            Console.WriteLine("Usuario: " + user1.FullName);
-
-            var user2 = new User
-            {
-                FirstName = "Ofelia",
-                LastName = "La del abarrote",
-                Email = "ofelia@abarrote.santa.fe",
-                Name = "ofelia",
-                Password = "123"
-            };
-
-            Console.WriteLine("Usuario: " + user2.FullName);
-            Console.Read();
-
+            Application.EnableVisualStyles();
+            Application.Run(new Form2());
         }
     }
 }
