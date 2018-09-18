b77db731bf44cd139a9aac4ed1e8349373a258f1
bidkar <bidkar.aragon@udo.mx>
2018-09-18 11:58:48 -0600

    manejo de numeros
    conversion de string a numericos

---
+++ HolaUADEO/Form4.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace HolaUADEO
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUno = new System.Windows.Forms.TextBox();
+            this.txtDos = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUno
+            // 
+            this.txtUno.Location = new System.Drawing.Point(55, 36);
+            this.txtUno.Name = "txtUno";
+            this.txtUno.Size = new System.Drawing.Size(100, 20);
+            this.txtUno.TabIndex = 0;
+            // 
+            // txtDos
+            // 
+            this.txtDos.Location = new System.Drawing.Point(55, 79);
+            this.txtDos.Name = "txtDos";
+            this.txtDos.Size = new System.Drawing.Size(100, 20);
+            this.txtDos.TabIndex = 1;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(184, 34);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(28, 23);
+            this.btnSuma.TabIndex = 2;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Num1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Num2";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Location = new System.Drawing.Point(12, 203);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(317, 72);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "label3";
+            // 
+            // btnResta
+            // 
+            this.btnResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResta.Location = new System.Drawing.Point(229, 34);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(28, 23);
+            this.btnResta.TabIndex = 6;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivision.Location = new System.Drawing.Point(229, 82);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(28, 23);
+            this.btnDivision.TabIndex = 8;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicacion.Location = new System.Drawing.Point(184, 82);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(28, 23);
+            this.btnMultiplicacion.TabIndex = 7;
+            this.btnMultiplicacion.Text = "*";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 284);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtDos);
+            this.Controls.Add(this.txtUno);
+            this.Name = "Form4";
+            this.Text = "Manejo de Números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUno;
+        private System.Windows.Forms.TextBox txtDos;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnMultiplicacion;
+    }
+}
---
+++ HolaUADEO/Form4.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaUADEO
+{
+    public partial class Form4 : Form
+    {
+        private double num1, num2;
+
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private bool textBoxIsNotEmpty()
+        {
+            if (!string.IsNullOrEmpty(txtUno.Text) && !string.IsNullOrEmpty(txtDos.Text))
+                return true;
+            else
+                return false;
+        }
+
+        private bool valuesCanParseToDouble()
+        {
+            try
+            {
+                num1 = double.Parse(txtUno.Text);
+                num2 = double.Parse(txtDos.Text);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            // TODO: validar que existan valores en los cuadros de textos
+            if (textBoxIsNotEmpty() && valuesCanParseToDouble())
+            {
+                // TODO: validar que los valores existentes sean numericos
+                lblResultado.Text = (num1 + num2).ToString();
+                // TODO: realizar la sumatoria y mostrar el resultado en la etiqueta lblResultado
+            }
+            else
+            {
+                lblResultado.Text = "Los datos son obligatorios y deben ser numericos";
+            }
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            // TODO: validar que existan valores en los cuadros de textos
+            if (textBoxIsNotEmpty() && valuesCanParseToDouble())
+            {
+                // TODO: validar que los valores existentes sean numericos
+                lblResultado.Text = (num1 - num2).ToString();
+                // TODO: realizar la sumatoria y mostrar el resultado en la etiqueta lblResultado
+            }
+            else
+            {
+                lblResultado.Text = "Los datos son obligatorios y deben ser numericos";
+            }
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            // TODO: validar que existan valores en los cuadros de textos
+            if (textBoxIsNotEmpty() && valuesCanParseToDouble())
+            {
+                // TODO: validar que los valores existentes sean numericos
+                lblResultado.Text = (num1 * num2).ToString();
+                // TODO: realizar la sumatoria y mostrar el resultado en la etiqueta lblResultado
+            }
+            else
+            {
+                lblResultado.Text = "Los datos son obligatorios y deben ser numericos";
+            }
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            // TODO: validar que existan valores en los cuadros de textos
+            if (textBoxIsNotEmpty() && valuesCanParseToDouble())
+            {
+                // TODO: validar que los valores existentes sean numericos
+                lblResultado.Text = (num1 / num2).ToString();
+                // TODO: realizar la sumatoria y mostrar el resultado en la etiqueta lblResultado
+            }
+            else
+            {
+                lblResultado.Text = "Los datos son obligatorios y deben ser numericos";
+            }
+        }
+    }
+}
